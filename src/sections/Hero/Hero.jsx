3307d399c134f5fc666ed3f865d568950bc8fd9a
DylanPatrascu <dylanpatrascu1@gmail.com>
2025-04-29 18:25:38 -0400

    Added coloured vectors

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,11 +1,12 @@
 import styles from "./HeroStyles.module.css"
 import heroImage from "../../assets/Headshot.png"
-import linkedin from "../../assets/linkedin-light.svg"
-import github from "../../assets/github-light.svg"
-import itchio from "../../assets/itch-light.svg"
+import { ReactComponent as LinkedInIcon } from "../../assets/linkedin.svg";
+import { ReactComponent as GitHubIcon } from "../../assets/github.svg";
+import { ReactComponent as ItchIcon } from "../../assets/itch.svg";
+import { ReactComponent as DownArrow } from "../../assets/downArrow.svg";
 import resume from "../../assets/DylanPatrascuResume.pdf"
-import scrollGif from "../../assets/mouse-scroll.gif"
 import setThemeVars from '../../common/SetTheme';
+
 
 function Hero() {
   return (
@@ -20,17 +21,18 @@ function Hero() {
             <h1>Dylan<br/> Patrascu</h1>
             <h2>Game Developer</h2>
             <span>
-                <a href="https://www.linkedin.com/in/dylanpatrascu" target="_blank"> <img src={linkedin} alt="LinkedIn Icon"/></a>
-                <a href="https://github.com/dylanpatrascu" target="_blank"> <img src={github} alt="GitHub Icon"/></a>
-                <a href="https://dylanpatrascu.itch.io" target="_blank"> <img src={itchio} alt="Itch.io Icon"/></a>
+                <a href="https://www.linkedin.com/in/dylanpatrascu" target="_blank"><LinkedInIcon className={styles.icon} /></a>
+                <a href="https://github.com/dylanpatrascu" target="_blank"><GitHubIcon className={styles.icon} /></a>
+                <a href="https://dylanpatrascu.itch.io" target="_blank"><ItchIcon className={styles.icon} /></a>
             </span>
             <p className={styles.description}>A computer science graduate with a passion for game development.</p>
             <a href={resume} target="_blank">
                 <button className="hover">Resume</button>
             </a>
+            <DownArrow className={`${styles.arrow} ${styles.wiggle}`} />
+
         </div>
-        {/* <img className={styles.scrollGif} src={scrollGif} alt="Scroll indicator"/> */}
-    </section>
+        </section>
   )
 }
 
